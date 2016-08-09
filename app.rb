83e8e1485a3c587f8907552cba577e6a69de8e44
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-09 14:35:22 +0000

    second commit

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'json'
-require 'activerecord'
+require 'active_record'
 require 'sinatra/activerecord'
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://amfzhtdoyvixst:dyR7kstCVyDT6HCs3bY4cbGqAr@ec2-54-235-89-113.compute-1.amazonaws.com:5432/daeo4qlo2s0b9q')
