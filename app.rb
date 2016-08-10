24bc7562ec1a8422b70422b20f67e0d8368282a1
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-10 07:26:01 +0000

    add /lingr

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/activerecord'
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://amfzhtdoyvixst:dyR7kstCVyDT6HCs3bY4cbGqAr@ec2-54-235-89-113.compute-1.amazonaws.com:5432/daeo4qlo2s0b9q')
 
-class Count < ActiveRecord::Base; end
+class Item < ActiveRecord::Base; end
 
 get '/' do
   'Hello,World!'
@@ -17,5 +17,28 @@ get '/music' do
 end
 
 post '/music' do
+  music = Item.new
+  music.title  = params[:title]
+  music.artist = params[:artist]
+  music.genre  = params[:genre]
+  music.url    = params[:url]
+  music.save
+  redirect '/'
 end
 
+get '/show' do
+  @title = "Music Information List"
+  @items = Item.all
+  erb :show
+end
+
+post '/lingr' do
+  j = JSON.parse(request.body.string)
+  j['events'].select{|e| e['message']}.map{|e|
+    if e['message']['text'] == "music" then
+      Item.order('RANDOM()').limit(1).first.url
+      else
+        ""
+    end
+  }
+end
